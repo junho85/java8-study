bcc18f44163211bb80c0bbe4c6a0f47f676250d2
june.kim <junho85@gmail.com>
2021-12-29 21:07:38 +0900

    init project

---
+++ src/main/java/Random/SomeRandom.java
@@ -0,0 +1,15 @@
+package Random;
+
+import java.util.Random;
+
+public class SomeRandom {
+    static void test() {
+        Random random = new Random();
+        System.out.println(random.nextInt());
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            test();
+        }
+    }
+}
