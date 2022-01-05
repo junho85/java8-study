70d09bb8237cb66d7a7da55ada0979f9a3135351
june.kim <junho85@gmail.com>
2022-01-05 23:56:39 +0900

    Random, StringConcat

--- src/main/java/Random/SomeRandom.java
+++ src/main/java/Random/SomeRandom.java
@@ -5,7 +5,11 @@ import java.util.Random;
 public class SomeRandom {
     static void test() {
         Random random = new Random();
+        Random random2 = new Random();
+        Random random3 = new Random();
         System.out.println(random.nextInt());
+        System.out.println(random2.nextInt());
+        System.out.println(random3.nextInt());
     }
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
---
+++ src/main/java/String/StringConcat.java
@@ -0,0 +1,13 @@
+package String;
+
+public class StringConcat {
+    public static void main(String[] args) {
+        String lines = "line1\n"
+                + "line2\n"
+                + "line3\n";
+
+        String test1 = "hello";
+        String test2 = "world";
+        String test3 = test1 + " " + test2;
+    }
+}
